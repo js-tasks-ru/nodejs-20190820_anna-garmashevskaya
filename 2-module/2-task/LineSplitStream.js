edf3d588057caa996aece1fd2367e218334b01cf
Anna Garmashevska <agarmashevska@gmail.com>
2019-08-27 11:41:26 +0300

    module 2 task 2

--- 2-module/2-task/LineSplitStream.js
+++ 2-module/2-task/LineSplitStream.js
@@ -4,12 +4,25 @@ const os = require('os');
 class LineSplitStream extends stream.Transform {
   constructor(options) {
     super(options);
+    this.options = options;
   }
 
   _transform(chunk, encoding, callback) {
+    const {encoding: code} = this.options;
+    let data = chunk.toString(code);
+    if (this.rest) data = this.rest + data;
+
+    const lines = data.split(os.EOL);
+    this.rest = lines.pop();
+
+    lines.forEach((line) => this.push(line));
+    callback();
   }
 
   _flush(callback) {
+    if (this.rest) this.push(this.rest);
+    this.rest = '';
+    callback();
   }
 }
 
